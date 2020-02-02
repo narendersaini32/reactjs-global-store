ae68b81e9f87c2eaff171c4bae894dacfe6852a3
Naruto <naruto@ryaz.io>
2020-02-02 09:29:47 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const Context = React.createContext();
+
+class ContextProvider extends Component {
+    constructor(props) {
+        super(props);
+        const { defaultState = {} } = props;
+        this.state = { defaultState };
+    }
+
+
+    updateContext = (data, callback = () => { }) => {
+        this.setState(data, () => {
+            if (callback) { callback(); }
+        });
+    }
+
+    render() {
+        const { children } = this.props;
+        return (
+            <Context.Provider
+                value={{ ...this.state, updateContext: this.updateContext }}
+            >
+                {children}
+            </Context.Provider>
+        );
+    }
+}
+
+ContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+    defaultState: PropTypes.instanceOf(Object)
+};
+
+ContextProvider.defaultProps = {
+    defaultState: {}
+}
+
+export { ContextProvider, Context };
