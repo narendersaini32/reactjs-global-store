28df527596a54e0e4808b2ba99dfdce43a04ea39
Naruto <naruto@ryaz.io>
2020-02-09 10:09:07 +0530

    restored

--- src/index.js
+++ src/index.js
@@ -1,69 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
 
-import CloseIcon from './closeIcon';
-import { blur } from './blur';
-import "./index.css";
+const Context = React.createContext();
 
-export class Dialog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleClickOutside);
-    document.body.style.overflow = 'hidden';
-    blur.add();
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
-    document.body.style.overflow = 'auto';
-    blur.remove();
-  }
-
-  handleClickOutside = event => {
-    const { className } = event.target;
-    if (
-      this.show &&
-      !this.show.contains(event.target) &&
-      className === 'dialog-wrapper'
-    ) {
-      const { onClose } = this.props;
-      onClose();
+class ContextProvider extends Component {
+    constructor(props) {
+        super(props);
+        const { defaultState = {} } = props;
+        this.state = defaultState ;
     }
-  };
 
-  setWrapperRef = node => {
-    this.show = node;
-  };
 
-  render() {
-    const { onClose, children, closeIcon } = this.props;
-    return ReactDOM.createPortal(
-      <div className="dialog-wrapper">
-        <div className="dialog" ref={this.setWrapperRef}>
-          {closeIcon && (
-            <div className="close-icon-wrapper">
-              <CloseIcon className="close-icon" onClick={onClose} />
-            </div>
-          )}
-          <div className="content">{children}</div>
-        </div>
-      </div>,
-      document.body,
-    );
-  }
+    updateContext = (data, callback = () => { }) => {
+        this.setState(data, () => {
+            if (callback) { callback(); }
+        });
+    }
+
+    render() {
+        const { children } = this.props;
+        return (
+            <Context.Provider
+                value={{ ...this.state, updateContext: this.updateContext }}
+            >
+                {children}
+            </Context.Provider>
+        );
+    }
 }
 
-Dialog.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired,
-  closeIcon: PropTypes.bool,
+ContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+    defaultState: PropTypes.instanceOf(Object)
 };
 
-Dialog.defaultProps = {
-  closeIcon: true,
-};
+ContextProvider.defaultProps = {
+    defaultState: {}
+}
+
+export { ContextProvider, Context };
